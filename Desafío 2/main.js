8710abd931b353326e66d35cfc22a6cce735aca6
gerfabris <ggerfabris@gmail.com>
2022-07-18 20:12:33 -0300

    Desafío 2

---
+++ Desafío 2/main.js
@@ -0,0 +1,9 @@
+const Contenedor = require("./contenedor");
+
+const contenedor = new Contenedor('./prueba.txt')
+
+//contenedor.save({nombre: "Mate Pampa", stock: 15, categoria: "Mate"})
+//contenedor.getById(); 
+//contenedor.deleteById(3)
+//contenedor.getAll()
+//contenedor.deleteAll()
